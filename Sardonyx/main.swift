0f9690ea7e94116b05f3aac849145a2cb2e7f19d
Andrey Volodin <siddok@gmail.com>
2020-06-12 22:25:06 +0300

    Initial Commit

---
+++ Sardonyx/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sardonyx
+//
+//  Created by Andrey Volodin on 12.06.2020.
+//  Copyright © 2020 Andrey Volodin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

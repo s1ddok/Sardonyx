fa4965dcd29321ab05a5b05ecf331152ce349537
Andrey Volodin <siddok@gmail.com>
2020-06-27 17:52:06 +0300

    temporary hack for reshape conversion

--- Sardonyx/NodeConverters/ReshapeConverter.swift
+++ Sardonyx/NodeConverters/ReshapeConverter.swift
@@ -8,7 +8,7 @@ class ReshapeConverter: NodeConverter {
     func contributeImplementation(using context: GenerationContext) {
         let outputname = self.node.output[0]
         
-        let shape = context.tensors[node.input[1]]!.int64Data.map(Int.init)
+        let shape = context.tensors[node.input[1]]!.int64Data.map(Int.init).filter { $0 != 0 }
         context.sourceBuilder.add(line: "let _\(outputname) = _\(self.node.input[0]).reshaped(to: [\(shape.map(String.init).joined(separator: ", "))])")
     }
 }

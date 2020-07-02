6c6aaace1bba7282ea68aeffbb45686dca92de77
Andrey Volodin <siddok@gmail.com>
2020-07-02 14:04:52 +0300

    bug fix readcount

--- Sardonyx/NodeConverters/MPS/AddMetalConverter.swift
+++ Sardonyx/NodeConverters/MPS/AddMetalConverter.swift
@@ -19,12 +19,15 @@ class AddMetalConverter: NodeConverter {
     }
     
     func contributeImplementation(using context: GenerationContext) {
+        context.sourceBuilder.add(line: "if _\(self.node.input[0]).featureChannels == 1 && _\(self.node.input[0]).featureChannels != _\(self.node.input[1]).featureChannels { self.add_\(self.node.name).primaryStrideInFeatureChannels = 0 } else { self.add_\(self.node.name).primaryStrideInFeatureChannels = 1 }")
+        context.sourceBuilder.add(line: "if _\(self.node.input[1]).featureChannels == 1 && _\(self.node.input[0]).featureChannels != _\(self.node.input[1]).featureChannels { self.add_\(self.node.name).secondaryStrideInFeatureChannels = 0 } else { self.add_\(self.node.name).secondaryStrideInFeatureChannels = 1 }")
+        
         context.sourceBuilder.add(line: "let _\(self.node.output[0]) = self.add_\(self.node.name).encode(commandBuffer: commandBuffer, primaryImage: _\(self.node.input[0]), secondaryImage: _\(self.node.input[1]))")
         
         let readCount = context.readCounts[self.node.output[0], default: 0]
         
         if readCount > 1 {
-            context.sourceBuilder.add(line: "(_\(self.node.output[0]) as? MPSTemporaryImage)?.readCount = 2")
+            context.sourceBuilder.add(line: "(_\(self.node.output[0]) as? MPSTemporaryImage)?.readCount = \(readCount)")
         }
         
     }
